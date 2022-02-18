01a39a71b1f63190003e3b9342cd8e2f2e45d2fa
Bryce Schuebert <bryceschuebert@gmail.com>
2022-02-18 12:30:02 -0500

    Public Folder added

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Bryce Schuebert',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Bryce Schuebert is a mechanical engineer designing, fabricating, and delivering bespoke products',
+    siteUrl: 'https://bryceschuebert.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@bryceschuebert',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
